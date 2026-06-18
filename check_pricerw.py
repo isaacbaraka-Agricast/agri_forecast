574ff7db084e0b3db79c3ff851044a7474f904c1
Baraka Isaac <isaacbaraka60@gmail.com>
2026-06-19 00:51:48 +0200

    fix: price tip_rw uses Kinyarwanda crop name (name_rw)

---
+++ check_pricerw.py
@@ -0,0 +1,5 @@
+﻿with open("C:/xampp/htdocs/agri_forecast/app.py", "r", encoding="utf-8") as f:
+    content = f.read()
+
+idx = content.find("tip_rw")
+print(repr(content[idx:idx+300]))
---
+++ check_pricerw3.py
@@ -0,0 +1,9 @@
+﻿with open("C:/xampp/htdocs/agri_forecast/app.py", "r", encoding="utf-8") as f:
+    content = f.read()
+
+old = '                          f"igiciro kigera ku {max_price} RWF/kg.",'
+new = '                          f"igiciro kigera ku {max_price} RWF/kg. ({intel[\"name_rw\"]})",'
+
+# First check what's around the tip_rw line
+idx = content.find("Igihe cyiza cyo kugurisha {crop_name}")
+print(repr(content[idx-50:idx+200]))
